3a8315f69546c8ee7a224771ab058b1e4fb4e4b9
Roy Hyunjin Han <rhh@crosscompute.com>
2018-02-17 19:51:37 +0000

    Bump dependencies

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ DESCRIPTION = '\n\n'.join(open(join(FOLDER, x)).read().strip() for x in [
     'README.rst'])
 setup(
     name='crosscompute-video',
-    version='0.7.5',
+    version='0.7.6',
     description='Video data type plugin for CrossCompute',
     long_description=DESCRIPTION,
     classifiers=[
@@ -31,6 +31,6 @@ setup(
         'pytest-runner'
     ],
     install_requires=[
-        'crosscompute>=0.7.5',
+        'crosscompute>=0.7.6',
     ],
     entry_points=ENTRY_POINTS)
